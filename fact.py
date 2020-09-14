f13a31d63b0458e6730feb83b7347d5176b99e4d
shiva1206 <61496636+shiva1206@users.noreply.github.com>
2020-09-14 19:53:20 +0530

    Create fact.py

---
+++ fact.py
@@ -0,0 +1,12 @@
+factTable={}
+def factorial(n):
+      try:
+            return factTable[n]
+      except KeyError:
+            if n==0:
+                  factTable[0]=1
+                  return 1
+            else:
+                  factTable[n]=n*factorial(n-1)
+                  return factTable[n]
+print(factorial(10))
